43528fc1218165c07c6e9c825dce0dcbeef62f23
Carlos Alberto González Montoya <104763487+cagmspain@users.noreply.github.com>
2022-10-04 22:53:50 +0200

    buger menu done

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,23 @@
+/*
+Funcion para pedir datos de posts a jsonplaceholder
+
+*/
+
 const getData = async () => {
 	const promise = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
 	//console.log(promise);
 	const data = await promise.json();
-	console.log(data[0].title);
+	//console.log(data[0].title);
 	return data;
 };
 //console.log(getData(3));
 
+//Función para insertar datos en el DOM
+//Gestionar titulos y contenidos del texto
+
 const projectsTitles = async () => {
 	const data = await getData();
-	console.log(data);
+	//console.log(data);
 	const title1 = data[0].title;
 	const title2 = data[1].title;
 	const title3 = data[2].title;
@@ -17,8 +25,8 @@ const projectsTitles = async () => {
 	const body2text = data[1].body;
 	const body3text = data[2].body;
 
-	console.log(title1, title2, title3);
-	console.log(body1text, ".....", body2text, "....3", body3text);
+	//console.log(title1, title2, title3);
+	//console.log(body1text, ".....", body2text, "....3", body3text);
 
 	const titleDiv1 = document.querySelector("#title1");
 	const titleDiv2 = document.querySelector("#title2");
@@ -26,7 +34,7 @@ const projectsTitles = async () => {
 	const body1 = document.querySelector("#body1");
 	const body2 = document.querySelector("#body2");
 	const body3 = document.querySelector("#body3");
-	console.log(body1, body2);
+	//console.log(body1, body2);
 	titleDiv1.innerHTML = `${title1}`;
 	titleDiv2.innerHTML = `${title2}`;
 	titleDiv3.innerHTML = `${title3}`;
@@ -35,3 +43,20 @@ const projectsTitles = async () => {
 	body3.innerHTML = `${body3text}`;
 };
 projectsTitles();
+
+//Burguer menu
+// Desplegar menu hamburguesa posterior al evento click
+//version movil
+
+const burguer = document.querySelector(".fa-solid");
+console.log(burguer);
+const navMobile = document.querySelector(".not-desktop");
+console.log(navMobile);
+burguer.addEventListener("click", () => {
+	console.log("im here");
+	navMobile.removeAttribute("hidden");
+});
+navMobile.addEventListener("click", () => {
+	console.log("hello");
+	navMobile.setAttribute("hidden", true);
+});
